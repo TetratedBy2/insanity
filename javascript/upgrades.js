f0f5cc603ab84746e786ba208ff4594d8e521bcd
J.U.M.P.E.R. <59283818+TetratedBy2@users.noreply.github.com>
2025-07-25 14:42:37 +0200

    Update upgrades.js

--- javascript/upgrades.js
+++ javascript/upgrades.js
@@ -2,6 +2,7 @@ function buyDollarUpgrade(index) {
   if (game.dollars.gte(game.dollarUpgrades[index].upg_cost)) {
     game.dollarUpgrades[index].upg.add(1);
     game.dollarUpgrades[index].upg_cost.mul(2);
+    document.getElementById("dollarUpg"+ index +"Count").innerText = game.dollarUpgrades[index].upg;
     document.getElementById("dollarUpg"+ index +"Cost").innerText = game.dollarUpgrades[index].upg_cost;
   }
 }
