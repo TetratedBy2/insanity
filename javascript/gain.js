4d575b77da42f62540f3adb83abfad1d2a45f796
J.U.M.P.E.R. <59283818+TetratedBy2@users.noreply.github.com>
2025-07-25 14:47:32 +0200

    Update gain.js

--- javascript/gain.js
+++ javascript/gain.js
@@ -1,5 +1,6 @@
 function calculateDollarGain() {
   let gain = new ExpantaNum(1);
+  if (game.dollarUpgrades[0].upg.gt(0)) gain.add(game.dollarUpgrades[0].upg);
   game.dollars = game.dollars.add(gain.div(50));
 }
 
